d358cd9f588af78d70b2260b7b7fd4b195eac5c8
Md Mehedi Hasan Shanto <ll4146394@gmail.com>
2026-08-01 19:18:25 +0600

    speling mistake was in my dlete routes now i corretion it

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ async function server() {
     })
 
 
-    app.get('/rooms/:id', async (req, res) => {
+    app.delete('/rooms/:id', async (req, res) => {
         const id = req.params.id;
         const query = { _id: new ObjectId(id) };
         const result = await addRoomCollection.deleteOne(query);
