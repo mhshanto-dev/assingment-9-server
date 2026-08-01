16787f7833f139a3e2518f6fff53fd0be5cf55cd
Md Mehedi Hasan Shanto <ll4146394@gmail.com>
2026-08-01 12:12:39 +0600

    create post method /addroom routes and send resposne to client now we will work in client side so lets get started

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv").config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 const cors = require("cors");
 
 app.use(express.json());
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri =process.env.DB_URI;
+const uri =process.env.MONGODB_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -33,13 +33,22 @@ async function server() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    // Work is start from hare 
+    const db = client.db("studynook");
+    const addRoomCollection = db.collection("addroom");
+
+    app.post("/addroom", async (req, res) => {
+        const addRoom = req.body;
+        const result = await addRoomCollection.insertOne(addRoom);
+        res.send(result);
+    })
+    
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-
-    
-
-    
-
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
